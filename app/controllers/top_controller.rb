d921df5f47e2548046d9ba2ded3f76478d26c836
yuya <yu.murata6@gmail.com>
2022-07-09 19:51:03 +0900

    chapter4 fin

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
   def index
+    # ここに追加
+    @lists = List.where(user: current_user).order("created_at ASC")
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  belongs_to :user
+
+  # ここに追加
+  validates :title, length: { in: 1..255 }
 end
