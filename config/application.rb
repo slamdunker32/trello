b7d03c5415e2b3f3fe11d8e33b1b5096633401dc
yuya <yu.murata6@gmail.com>
2022-07-10 18:24:38 +0900

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Kanban
 
     # ここに追加する
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
